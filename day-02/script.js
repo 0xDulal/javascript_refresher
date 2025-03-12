e42d16d2cf06a16842a70887758f2f6208e75be2
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-03-12 17:17:19 +0600

    dudu

--- day-02/script.js
+++ day-02/script.js
@@ -26,3 +26,27 @@ let amarmon = function(name){
 };
 amarmon("Dulal");
 amarmon("Dulal");
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+let seqNumber = numbers.map(function(number){
+      return number * number;
+});
+console.log(seqNumber);
+console.log(numbers[0]);
+
+//arrow function
+let newArrow = (a, b) => {
+      return a + b;
+}
+console.log(newArrow(10, 20));
+
+
+function kodumia(gali) {
+      function ageDak(){
+            alert("Tore koto bar bolsi " + gali);
+      }
+      return ageDak();
+}
+kodumia("Bolod");
+
